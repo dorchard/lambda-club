50a12227661bd4e7b80674690ed2389a8f586d14
dominic <dom.orchard@gmail.com>
2017-03-01 13:56:20 +0000

    tweak

--- sierpinsky/dom.hs
+++ sierpinsky/dom.hs
@@ -5,6 +5,7 @@ sierpinski n h = mapM_ putStrLn $ sierpinski' n h
 sierpinski' n h = edge h +++ sp n h '1' +++ edge h
    where edge h = [replicate n '_' | n <- [h-1,h-2..0]]
 
+sp :: Int -> Int -> Char -> [String]
 sp 0 h c = [replicate (r*2 + 1) c | r <- [0..(h-1)]]
 sp n h c = t ++ (t +++ f +++ t)
   where
