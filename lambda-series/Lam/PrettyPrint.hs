6cb907b045167f0c2c323b86fb9818d80799f714
Hrutvik Kanabar <hrutvikkanabar@yahoo.co.uk>
2019-10-03 15:59:19 +0100

    Lambda core simple pretty printer

--- lambda-series/Lam/PrettyPrint.hs
+++ lambda-series/Lam/PrettyPrint.hs
@@ -5,7 +5,20 @@ import Lam.Syntax
 
 -- Pretty print terms
 class PrettyPrint t where
+    isLexicallyAtomic :: t -> Bool
     pprint :: t -> String
 
+bracket_pprint :: PrettyPrint t => t -> String
+bracket_pprint t | isLexicallyAtomic t = pprint t
+                 | otherwise           = "(" ++ pprint t ++ ")"
+
+-- Untyped lambda calculus
 instance PrettyPrint (Expr ex) where
-    pprint = todo "pprint expr"
+    isLexicallyAtomic (Var _) = True
+    isLexicallyAtomic _       = False
+
+    pprint (Abs var e) = "\\" ++ var ++ " -> " ++ pprint e
+    pprint (App (Abs var e1) e2) =
+      bracket_pprint (Abs var e1) ++ " " ++ bracket_pprint e2
+    pprint (App e1 e2) = pprint e1 ++ " " ++ bracket_pprint e2
+    pprint (Var var) = var
