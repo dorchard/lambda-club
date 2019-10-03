6036a6fca7b191b0c076edd30ffd6ca04c21cdd4
Hrutvik Kanabar <hrutvikkanabar@yahoo.co.uk>
2019-10-03 15:58:47 +0100

    Lambda core bound/free variables, fresh variable names

--- lambda-series/Lam/Syntax.hs
+++ lambda-series/Lam/Syntax.hs
@@ -1,5 +1,7 @@
 module Lam.Syntax where
 
+import Data.Set
+
 type Id = String
 
 -- Abstract-syntax tree for LambdaCore
@@ -23,3 +25,24 @@ data Arith ex =
   | Conditional (Expr (Arith ex)) (Expr (Arith ex)) (Expr (Arith ex))
   | Constant Int
   deriving (Show, Eq)
+
+----------------------------
+-- Bound and free variables
+
+bound_vars :: Expr ex -> Set Id
+bound_vars (Abs var e) = var `insert` bound_vars e
+bound_vars (App e1 e2) = bound_vars e1 `union` bound_vars e2
+bound_vars (Var var) = singleton var
+
+free_vars :: Expr ex -> Set Id
+free_vars (Abs var e) = delete var $ free_vars e
+free_vars (App e1 e2) = free_vars e1 `union` free_vars e2
+free_vars (Var var) = singleton var
+
+----------------------------
+-- Fresh variable with respect to a set of variables
+-- By adding apostrophes to a supplied initial variable
+
+fresh_var :: Id -> Set Id -> Id
+fresh_var var vars =
+  if var `member` vars then fresh_var (var ++ "'") vars else var
