b8e327000e489d1d985aa489407cc4b421c33e02
dominic <dom.orchard@gmail.com>
2019-10-31 19:43:39 +0000

    tidy up comments; add colors

--- lambda-series/Lam/Types.hs
+++ lambda-series/Lam/Types.hs
@@ -5,6 +5,12 @@ import Lam.PrettyPrint
 
 {-
 
+*************************************************************
+Declarative specification of the simply-typed lambda calculus
+*************************************************************
+Recall contexts are like lists of variable-type assumptions
+
+
 G ::=  G, x : A | .
 
        (x : A) in G
@@ -19,71 +25,98 @@ app ---------------------------------------
 abs ------------------------
       G |- \x -> e : A -> B
 
+-}
+
+-- Represent contexts as lists
+type Context = [(Identifier, Type)]
+
+{-
+
+Bidirectional checking
 *********************************
 G |- e <= A    check
 **********************************
+-}
+
+check :: Context -> Expr PCF -> Type -> Bool
+
+{--
 
 G, x : A |- e <= B
 --------------------------- abs
 G |- (\x -> e) <= A -> B
 
+-}
+check gamma (Abs x expr) (FunTy tyA tyB) =
+  check ([(x, tyA)] ++ gamma) expr tyB
+
+{--
+
 G |- e => A'   A' == A
 --------------------------- synthCheck
 G |- e <= A
 
-**********************************
-G |- e => A    synth
-**********************************
+--}
 
-(x : A) in G
---------------- var
-G |- x => A
-
-G |- e1 => A -> B    G |- e2 <= A
------------------------------------ app
-G |- e1 e2 => B
-
-G |- e <= A
-------------------- checkSynth
-G |- (e : A) => A
-
-[IF CHURCH SYNTAX, but NOT IN LCORE/CURRY SYNTAX]
-G, x : A |- e => B
---------------------------- abs
-G |- (\(x : A) -> e) => B
-
--}
-
-type Context = [(Identifier, Type)]
-
-check :: Context -> Expr PCF -> Type -> Bool
-
--- abs
-check gamma (Abs x expr) (FunTy tyA tyB) =
-  check ([(x, tyA)] ++ gamma) expr tyB
-
--- synthCheck
 check gamma expr tyA =
   case synth gamma expr of
     Nothing -> False
     Just tyA' -> tyA == tyA'
 
+{-
+Bidirectional synthesis
+**********************************
+ G |- e => A    synth
+**********************************
+-}
+
 synth :: Context -> Expr PCF -> Maybe Type
 
--- var
+{-
+
+(x : A) in G
+--------------- var
+G |- x => A
+
+-}
+
 synth gamma (Var x) =
   lookup x gamma
 
--- zero
+-- zero (we will add more rules for PCF soon)
 synth gamma (Ext Zero) =
   Just NatTy
+
+{-
+
+The following is a special form of (app) which
+is useful for doing top-level definitions in our style,
+which are of the form (\x -> e) (e' : A).
+
+This is equivalent to combining the synthesis for general
+application (below, (app) rule) with the synthesis rule we can have
+if we have Church-style syntax
+
+      G, x : A |- e => B
+      -------------------------------------- abs-Church
+      G |- (\(x : A) -> e) => A -> B
+
+i.e., we know we have a signature for the argument.
+
+-}
 
 -- app (special for form of top-level definitions)
 synth gamma (App (Abs x e1) (Sig e2 tyA)) =
    synth ([(x, tyA)] ++ gamma) e1
 
+{-
 
--- app
+  G |- e1 => A -> B    G |- e2 <= A
+  ----------------------------------- app
+  G |- e1 e2 => B
+
+-}
+
 synth gamma (App e1 e2) =
   -- Synth the left-hand side
   case synth gamma e1 of
@@ -100,12 +133,20 @@ synth gamma (App e1 e2) =
     Nothing ->
       error $ "Expecting (" ++ pprint e1 ++ ") to have function type."
 
+
+{-
+
+  G |- e <= A
+  ------------------- checkSynth
+  G |- (e : A) => A
+
+-}
+
 -- checkSynth
 synth gamma (Sig e ty) =
   if check gamma e ty
     then Just ty
     else error $ "Trying to check (" ++ pprint e ++ ") against " ++ pprint ty
-
 
 -- catch all (cannot synth here)
 synth gamma e =
--- lambda-series/Main.hs
+++ lambda-series/Main.hs
@@ -21,23 +21,35 @@ main = do
       -- Check if this is a file
       exists <- doesPathExist fname
       if not exists
-        then putStrLn $ "Fil `" <> fname <> "` cannot be found."
+        then putStrLn $ "File `" <> fname <> "` cannot be found."
         else do
           -- Read the file, parse, and do something...
           input <- readFile fname
           case parseProgram fname input of
             Right (ast, options) -> do
               -- Show AST
-              putStrLn $ "\n AST: " <> show ast
+              putStrLn $ "\n " <> ansi_bold <> "AST: " <> ansi_reset <> show ast
+
               -- Pretty print
-              putStrLn $ "\n Pretty: " <> pprint ast
+              putStrLn $ "\n " <> ansi_bold <> "Pretty: " <> ansi_reset <> pprint ast
+
               -- Evaluate
               let (normalForm, count) = multiStep ast
-              putStrLn $ "\n Number of steps: " <> show count
-              putStrLn $ "\n Normal form: " <> pprint normalForm
+              putStrLn $ "\n " <> ansi_bold <> "Number of steps: " <> ansi_reset <> show count
+              putStrLn $ "\n " <> ansi_bold <> "Normal form: " <> ansi_reset <> pprint normalForm
+
               -- Typing
               when (isTyped options)
-                  (case synth [] ast of
-                     Nothing -> putStrLn "\n Not well-typed!"
-                     Just ty -> putStrLn $ "\n Type is: " <> pprint ty)
+                (case synth [] ast of
+                   Nothing -> putStrLn $ "\n " <> ansi_bold <> ansi_red
+                                               <> "Not well-typed.\n" <> ansi_reset
+                   Just ty -> putStrLn $ "\n " <> ansi_bold <> ansi_green
+                                               <> "Well-typed " <> ansi_reset
+                                               <> ansi_bold <> "as " <> ansi_reset <>pprint ty)
             Left msg -> error msg
+
+ansi_red, ansi_green, ansi_reset, ansi_bold :: String
+ansi_red   = "\ESC[31;1m"
+ansi_green = "\ESC[32;1m"
+ansi_reset = "\ESC[0m"
+ansi_bold  = "\ESC[1m"
